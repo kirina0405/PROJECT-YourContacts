5fdec8952a4bff9d468968182c2d09eed12a586b
Iryna Pikalova <kirina0405@gmail.com>
2017-11-03 11:01:13 +0200

    Added all-components component-detail app-routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AllContactsComponent } from './all-contacts/all-contacts.component';
+import { ContactDetailComponent } from './contact-detail/contact-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllContactsComponent,
+    ContactDetailComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/interfaces/contact.ts
@@ -0,0 +1,10 @@
+export interface Contact {
+    id: number;
+    firstName: string;
+    secondName: string;
+    phone?: string;
+    birthday?: string;
+    website?: string;
+    email?: string;
+    company?: string;
+}
